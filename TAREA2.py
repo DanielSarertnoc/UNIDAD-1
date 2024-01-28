f0b84f29c028559200fcbf2e418a83aa65704727
Daniel Alessandro Contreras Hernandez <126816275+DanielSarertnoc@users.noreply.github.com>
2024-01-27 22:50:07 -0600

    Add files via upload

---
+++ TAREA2.py
@@ -0,0 +1,22 @@
+print("¡HOLA MUNDO!")
+
+    
+texto = "¡HOLA MUNDO!"
+print(texto)
+
+
+nombre = input("Ingrese su nombre: ")
+print("¡Hola",nombre,"!")
+
+
+print("OPERACIÓN ARITMÉTICA")
+resultado = ((3+2)/(2*5))**2
+print("El resultado de la operación aritmética es: ", resultado)
+
+
+print("NÚMERO DE HORAS TRABAJADAS")
+horasT = float(input("Ingrese el número de horas trabajadas en el día: "))
+costoH = float(input("Ingrese el precio en quetzales por cada hora trabajada: "))
+total=  horasT*costoH
+
+print("El total a pagar es de: Q",total)
